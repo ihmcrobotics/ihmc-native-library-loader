f6a92ae0d3e96b6f49bc0b34c2661d159e9d7728
Jesper Smith <jesper@halodi.com>
2020-03-27 14:28:14 +0100

    Added optional list of dependencies to be added to a library

--- src/main/java/us/ihmc/tools/nativelibraries/NativeLibraryDescription.java
+++ src/main/java/us/ihmc/tools/nativelibraries/NativeLibraryDescription.java
@@ -18,8 +18,39 @@ public interface NativeLibraryDescription
     * Get a list of libraries to load for current platform. Loading multiple libraries is especially
     * useful for Windows, as you don't have to muck around with RPATHS.
     * 
+    * Deprecated: Use getLibrariesWithDependencies() instead
+    * 
     * @param platform Get libraries for platform
     * @return List The libraries to load in order.
     */
-   public String[] getLibraries(Platform platform);
+   @Deprecated
+   default public String[] getLibraries(Platform platform)
+   {
+      return new String[0];
+   }
+   
+   
+   /**
+    * Get a list of libraries and their dependencies to load for current platform. 
+    * 
+    * On Windows, the dependencies are loaded in order. On other systems, you need to make sure to set the RPATH to $ORIGIN (Unix) or @rpath (MacOSX) to load the dependencies.
+    * 
+    * @param platform Get libraries for platform
+    * @return List The libraries to load in order, with their dependencies
+    */
+   default NativeLibraryWithDependencies[] getLibrariesWithDependencies(Platform platform)
+   {
+      String[] libraryNames = getLibraries(platform);
+      NativeLibraryWithDependencies[] libraries = new NativeLibraryWithDependencies[libraryNames.length];
+      
+      for(int i = 0; i < libraries.length; i++)
+      {
+         libraries[i] = NativeLibraryWithDependencies.fromFilename(libraryNames[i]);
+      }
+      
+      return libraries;
+      
+   }
+   
+   
 }
