54cf229884c2025e0ad6f53ec67db09cf15372d7
Jesper Smith <jesper@halodi.com>
2022-04-28 11:01:05 +0200

    Updated test

--- src/test/java/us/ihmc/tools/nativelibraries/DefaultNativeLibraryDescriptionTest.java
+++ src/test/java/us/ihmc/tools/nativelibraries/DefaultNativeLibraryDescriptionTest.java
@@ -2,6 +2,7 @@ package us.ihmc.tools.nativelibraries;
 
 import org.apache.commons.lang3.SystemUtils;
 
+import us.ihmc.tools.nativelibraries.NativeLibraryDescription.Architecture;
 import us.ihmc.tools.nativelibraries.NativeLibraryDescription.OperatingSystem;
 
 import org.junit.jupiter.api.Test;
@@ -24,19 +25,19 @@ public class DefaultNativeLibraryDescriptionTest
       
       if (SystemUtils.IS_OS_WINDOWS)
       {
-         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.WIN64)[0].getLibraryFilename();
+         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.WIN64, Architecture.x64)[0].getLibraryFilename();
          System.out.println(windowsCorrectName + " =? " + actualName);
          assertEquals(windowsCorrectName, actualName, "Library name not correct on Windows.");
       }
       else if (SystemUtils.IS_OS_MAC)
       {
-         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.MACOSX64)[0].getLibraryFilename();
+         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.MACOSX64, Architecture.x64)[0].getLibraryFilename();
          System.out.println(macCorrectName + " =? " + actualName);
          assertEquals(macCorrectName, actualName, "Library name not correct on Mac.");
       }
       else if (SystemUtils.IS_OS_LINUX)
       {
-         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.LINUX64)[0].getLibraryFilename();
+         String actualName = defaultNativeLibraryDescription.getLibrariesWithDependencies(OperatingSystem.LINUX64, Architecture.x64)[0].getLibraryFilename();
          System.out.println(linuxCorrectName + " =? " + actualName);
          assertEquals(linuxCorrectName, actualName, "Library name not correct on Linux.");
       }
