8bc85dc04617ad9eecc8a2825e51d1bdff3f1320
Dexton Anderson <danderson@ihmc.org>
2024-08-09 13:01:44 -0500

    :bookmark: 2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "2.0.2"
+   version = "2.0.3"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-native-library-loader"
    openSource = true
 
